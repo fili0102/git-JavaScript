cb2908edaf23ec44817407720ea14b9073eae424
cameronfilion <fili0102@algonquinlive.com>
2020-01-09 18:08:45 -0500

    fixed math issue

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var name = 'Bameron';
 var number = 100;
 
 console.log(`Hello My Name is ${name}!`);
-console.log(number * "2");
-console.log(number * "5" / number - "7");
+console.log(number * 2);
+console.log(number * 5 / number - 7);
